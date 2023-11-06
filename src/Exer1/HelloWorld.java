0137b800ad1f5e45943d45b59d4170c404eeb6a2
Njao <njao@live.com>
2023-11-06 13:43:06 +0100

    hello

---
+++ src/Exer1/HelloWorld.java
@@ -0,0 +1,16 @@
+package Exer1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        //du är en korvlåda!
+        /* som inte är en smurf
+        men en lurf =)
+         */
+        System.out.println("I'm taking my longboard out its my favorite lullaby rolling down by the oceanside!");
+        System.out.println("Snoopy is the best dog ever!");
+        System.out.print("i am a wallflower");
+        System.out.print(" dont talk to me \"like\" that");
+        System.out.println("\nbackie \\");
+        System.out.println("hello");
+        }
+    }
