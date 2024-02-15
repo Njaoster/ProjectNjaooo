43447141d2c8e9a66234dacd516ec6bca4a6cd18
Njao <njao@live.com>
2024-02-15 16:10:44 +0100

    mina program =)

--- src/codingisfornerds/TheNarrator.java
+++ src/codingisfornerds/TheNarrator.java
@@ -2,16 +2,16 @@ package codingisfornerds;
 
 public class TheNarrator {
 
-    private static final String[] ENGLISH_ALPHABET = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ",", ".", "?", " "};
+    private static final String[] English_Symbols = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ",", ".", "?", " "};
 
-    private static final String[] MORSE_CODE = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----", "--..--", ".-.-.-", "..--..", " "};
+    private static final String[] Morse_Code = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----", "--..--", ".-.-.-", "..--..", " "};
 
     public static String translateEnglishToMorseCode(String englishText) {
         StringBuilder morseCode = new StringBuilder();
         for (char c : englishText.toLowerCase().toCharArray()) {
-            int index = getIndex(ENGLISH_ALPHABET, c);
+            int index = getIndex(English_Symbols, c);
             if (index != -1) {
-                morseCode.append(MORSE_CODE[index]).append(" ");
+                morseCode.append(Morse_Code[index]).append(" ");
             } else {
                 throw new IllegalArgumentException("SYNTAX ERROR: Symbol not found. Agent Smith is on his way!: " + c);
             }
@@ -19,14 +19,15 @@ public class TheNarrator {
         return morseCode.toString().trim();
     }
 
+
     public static String translateMorseCodeToEnglish(String morseCode) {
         StringBuilder englishText = new StringBuilder();
         String[] morseWords = morseCode.trim().split(" ");
         for (String morseWord : morseWords) {
             if (!morseWord.isEmpty()) {
-                int index = getIndex(MORSE_CODE, morseWord);
+                int index = getIndex(Morse_Code, morseWord);
                 if (index != -1) {
-                    englishText.append(ENGLISH_ALPHABET[index]);
+                    englishText.append(English_Symbols[index]);
                 } else {
                     throw new IllegalArgumentException("SYNTAX ERROR: Symbol not found in Morse code DUH!: " + morseWord);
                 }
@@ -36,6 +37,8 @@ public class TheNarrator {
         }
         return englishText.toString();
     }
+
+
 
     private static int getIndex(String[] array, char c) {
         for (int i = 0; i < array.length; i++) {
